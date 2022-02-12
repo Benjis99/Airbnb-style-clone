bf9fdd3f9db44eade664bd782d7a04a15e2560a2
Benjis99 <91055304+Benjis99@users.noreply.github.com>
2022-02-12 17:36:20 +0100

    added react router, cards and footer

---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p>© Hotel copycat! No rights Reserved</p>
+      <p>Privacy ● Terms ● Sitemap ● Company Demo</p>
+    </div>
+  );
+}
+
+export default Footer;
+
+//2:04:25
